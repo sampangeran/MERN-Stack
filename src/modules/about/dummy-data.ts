83a6b5fb7500221d478b7124697778241a5fd0ef
albugowy15 <kholidbughowi@gmail.com>
2023-01-26 12:48:04 +0700

    feat: about page (not completed)

---
+++ src/modules/about/dummy-data.ts
@@ -0,0 +1,20 @@
+import { FeatureCardProps } from "./components/FeatureCard";
+import { CardChecklist, Laptop, HandThumbsUp } from "react-bootstrap-icons";
+
+export const ListFeature = [
+  {
+    title: "01. Standar Kurikulum Internasional",
+    desc: "Torche menyediakan kurikulum teknik kimia terbaik dari 5 universitas terbaik di Indonesia yang digabungkan dengan 5 universitas terbaik di dunia.",
+    Icon: CardChecklist,
+  },
+  {
+    title: "02. Kelas Online",
+    desc: "Torche mengadakan kelas sesuai permintaan dengan platform rapat premium (Google Meet dan Zoom Meetings) dan materi materi yang terintegrasi ke akun Google Workspace Anda.",
+    Icon: Laptop,
+  },
+  {
+    title: "03. Dapat Dipercaya",
+    desc: "Torche menyediakan kurikulum teknik kimia terbaik dari 5 universitas terbaik di Indonesia yang digabungkan dengan 5 universitas terbaik di dunia.",
+    Icon: HandThumbsUp,
+  },
+];
---
+++ src/pages/about/index.ts
@@ -0,0 +1,3 @@
+import AboutPage from "@/modules/about/page/AboutPage";
+
+export default AboutPage;
